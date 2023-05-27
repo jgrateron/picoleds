14f0ce394d3d796c5ab97c3c4bcafab83cb6fc51
jairo <jgrateron@gmail.com>
2023-05-27 18:50:22 -0400

    add modificacion de patron por teclado

--- leds.c
+++ leds.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/multicore.h"
 #include "semaforo.h"
 #include "circular.h"
 #include "porcolor.h"
 
 #define MAX_LEDS 12
 
-const uint leds[] = {4,3,2,16,17,18,26,27,28,13,14,15};
+const char encendidos[][12] = 
+{
+{'1','1','1','1','1','1','1','1','1','1','1','1'},
+};
 
-void mostrarLeds(const char patron[][MAX_LEDS], int pasos, int tiempo){
+const char apagados[][12] = 
+{
+{'0','0','0','0','0','0','0','0','0','0','0','0'},
+};
+
+
+const uint leds[] = {4,3,2,16,17,18,26,27,28,13,14,15};
+const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+
+uint patron_actual = 1;
+uint velocidad = 250;
+
+void mostrarLeds(uint patron_anterior, const char matriz[][MAX_LEDS], int pasos, int tiempo){
     for (int i = 0; i < pasos; i++){
         for (int l = 0; l < MAX_LEDS; l++){
-            switch(patron[i][l]){
+            switch(matriz[i][l]){
                 case '0':
                     gpio_put(leds[l], 0);
                     break;
@@ -20,14 +36,51 @@ void mostrarLeds(const char patron[][MAX_LEDS], int pasos, int tiempo){
                     break;
             }
         }
+        if (patron_anterior != patron_actual){
+            return;
+        }
         sleep_ms(tiempo);
     }
 }
 
+/*
+*/
+void core1_entry()
+{
+    gpio_put(LED_PIN, 1);
+    sleep_ms(2000);
+    gpio_put(LED_PIN, 0);
+    sleep_ms(500);
+    while (true) {
+        switch(patron_actual){
+            case 0:
+                mostrarLeds(patron_actual, apagados, 1, velocidad);
+                break;
+            case 1:
+                mostrarLeds(patron_actual, encendidos, 1, velocidad);
+                break;
+            case 2:
+                mostrarLeds(patron_actual, semaforo, 34, velocidad);
+                break;
+            case 3:
+                mostrarLeds(patron_actual, porcolor,3, velocidad);
+                break;
+            case 4:
+                mostrarLeds(patron_actual, circular_a,12, velocidad);
+                break;
+            case 5:
+                mostrarLeds(patron_actual, circular_b,12, velocidad);
+                break;
+        }
+    }
+}
+/*
+*/
 int main()
 {
     stdio_init_all();
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    multicore_launch_core1(core1_entry);
+    
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     
@@ -36,20 +89,45 @@ int main()
         gpio_set_dir(leds[i], GPIO_OUT);
 
     }
-    gpio_put(LED_PIN, 1);
-    sleep_ms(2000);
-    gpio_put(LED_PIN, 0);
-    sleep_ms(500);
-    while (true) {
-        for (int i = 0; i < 2; i++){
-            mostrarLeds(semaforo, 34, 500);
-        }
-        for(int i = 0; i < 10; i++){
-            mostrarLeds(porcolor,3, 100);
-        }
-        for(int i = 0; i < 10; i++){
-            mostrarLeds(circular,12, 50);
+    while(true){
+        char c = getchar();
+        switch(c){
+            case '0':
+                patron_actual = 0;
+                velocidad = 250;
+                break;
+            case '1':
+                patron_actual = 1;
+                velocidad = 250;
+                break;
+            case '2':
+                patron_actual = 2;
+                velocidad = 250;
+                break;
+            case '3':
+                patron_actual = 3;
+                velocidad = 100;
+                break;
+            case '4':
+                patron_actual = 4;
+                velocidad = 50;
+                break;
+            case '5':
+                patron_actual = 5;
+                velocidad = 50;
+                break;
+            case '+':
+                velocidad += 5;
+                break;
+            case '-':
+                velocidad -= 5;
+                break;
+            default:
+                patron_actual = 0;
+                velocidad = 50;
+                break;
         }
     }
+
     return 0;
 }
